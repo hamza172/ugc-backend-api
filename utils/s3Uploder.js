6999eac4d33d021c826b96af7e89c4c07890189e
Hamza Muazzam Iqbal <hamzammuazzam172@gmail.com>
2024-08-06 06:47:04 +0500

    Errors fixed

--- utils/s3Uploder.js
+++ utils/s3Uploder.js
@@ -1,29 +1,27 @@
-// import { S3 } from 'aws-sdk';
 const { S3Client } = require("@aws-sdk/client-s3");
 const { Upload } = require("@aws-sdk/lib-storage");
-const { fromIni } = require("@aws-sdk/credential-providers");
-const { fromEnv } = require("@aws-sdk/credential-providers");
+const { fromIni, fromEnv } = require("@aws-sdk/credential-providers");
 
-const credentails = fromIni({ profile: "s3_login" });
+const credentials = fromIni({ profile: "s3_login" });
 
 const fileUpload = async (file) => {
-  if (!file) return null
-  // set Unique Name of file
+  if (!file) return null;
+
+  // Generate a unique name for the file
   const generateUniqueFileName = (originalFileName) => {
     const timestamp = new Date().getTime();
-    const randomString = Math.random().toString(36).substring(2, 8); // Generate a random string
+    const randomString = Math.random().toString(36).substring(2, 8);
     return `${timestamp}-${randomString}-${originalFileName}`;
   };
 
-  // bucket initialization
+  // Initialize the S3 bucket
   const s3BucketName = process.env.AWS_BUCKET_NAME;
   const s3BucketRegion = process.env.AWS_BUCKET_REGION;
   const s3 = new S3Client({ region: s3BucketRegion, credentials: fromEnv() });
 
-  // Store File
-  let profileImage = null;
-  if (file) {
-    const s3UploadParams = new Upload({
+  try {
+    // Set up the upload parameters
+    const uploadParams = new Upload({
       client: s3,
       params: {
         Bucket: s3BucketName,
@@ -32,10 +30,16 @@ const fileUpload = async (file) => {
         ContentType: file.mimetype,
       },
     });
-    await s3UploadParams.done();
-    profileImage = s3UploadParams;
+
+    // Perform the upload
+    const result = await uploadParams.done();
+
+    // Return the location of the uploaded file
+    return result.Location;
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    throw new Error('Error uploading file to S3');
   }
-  return profileImage.singleUploadResult.Location;
 };
 
 module.exports = fileUpload;
